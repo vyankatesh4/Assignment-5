cc15a3e528318835675d0019ed23bdc8f32419c0
vyankatesh dube <93048498+vyankatesh4@users.noreply.github.com>
2021-12-15 14:00:20 +0530

    Add files via upload

---
+++ A5 - OOPs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+class power:
+    def pow(self,x,n):
+        print("pow(",x,",",n,") =",x**n)
+p=power()
+x=int(input("Enter 'x' value : "))
+n=int(input("Enter 'n' value : "))
+p.pow(10,2)
+
+
+# In[ ]:
+
+
+
+
